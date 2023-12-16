c0aa7248bd4e4c9b7e2e9cb0d27da3d7bc0e9551
Owen Huang <williamowenwijaya191204@gmail.com>
2023-12-16 10:32:09 +0700

    getx_snackbar_08 membuat snackbar dengan getx

---
+++ getx_snackbar_08/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Get.snackbar(
+              "Hello",
+              "i am owen",
+              onTap: (snack) => Get.to(
+                OtherPage(),
+              ),
+              backgroundColor: Colors.blueGrey,
+              colorText: Colors.white,
+              icon: Icon(Icons.notifications_active),
+              shouldIconPulse: false,
+            );
+          },
+          child: Text("Snack Bar"),
+        ),
+      ),
+    );
+  }
+}
+
+class OtherPage extends StatelessWidget {
+  const OtherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
