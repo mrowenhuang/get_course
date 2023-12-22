e805860be20d2543d4ef6e6002cc5a8a56de0b3d
Owen Huang <williamowenwijaya191204@gmail.com>
2023-12-22 12:39:30 +0700

    getx_change_theme_16 mengubah theme dengan get.changetheme

---
+++ getx_change_theme_16/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.light(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              Get.changeTheme(
+                Get.isDarkMode ? ThemeData.light() : ThemeData.dark(),
+              );
+            },
+            icon: Icon(
+              Icons.color_lens,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
