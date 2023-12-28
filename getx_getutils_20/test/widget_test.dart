e9f4bf066990d5323df269f09b38d6d00cf64b0b
Owen Huang <williamowenwijaya191204@gmail.com>
2023-12-28 17:09:29 +0700

    getx_getutils_20 penggunaan utils yang dapat memeriksa apapun dan jenis data

---
+++ getx_getutils_20/test/widget_test.dart
@@ -0,0 +1,21 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:get/get.dart';
+
+
+void main () {
+  // print("Hello world");
+  print(GetUtils.camelCase('owen haung'));
+  print(GetUtils.capitalize("William owen wijaya"));
+  print(GetUtils.isEmail("Williamowenwijaya123@"));
+  print(GetUtils.capitalizeFirst("william owen wijaya"));
+}
+
+//  @ fungsi getutils dapat membantu dalam mengelola data getUtils memiliki banyak fungsi 
+
+// ^ getUtils.(...)
